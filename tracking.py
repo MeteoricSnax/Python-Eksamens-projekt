4c08b1b0f972b5d085680a6414e1b0b03edd9266
Albert Vandel <albertvandel@yahoo.dk>
2019-06-18 12:30:21 +0200

    added module for circle tracking

---
+++ tracking.py
@@ -0,0 +1,34 @@
+from collections import deque
+import numpy as np
+import imutils
+import cv2
+
+def track_circle(pts, frame, hsv, color_lower, color_upper):
+    mask = cv2.inRange(hsv,color_lower,color_upper)
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+        # only proceed if at least one contour was found
+    if len(cnts) > 0:
+       # find the largest contour in the mask, then use it to compute the minimum enclosing circle.
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        center = (int(x), int(y))
+        if radius > 10:
+           # draw the circle and centroid on the frame,
+           # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+        # update the points queue
+        pts.appendleft(center)
+            
+        # loop over the set of tracked points
+        for i in range(1, len(pts)):
+            # if either of the tracked points are None, ignore them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+ 
+            # otherwise, compute the thickness of the line and draw the connecting lines
+            thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
